7903cc38db4da48a9193e370db05e91469e0d1ce
OmerFarukD <omer.crlys.44@gmail.com>
2022-07-13 14:00:45 +0300

    Add project files.

---
+++ EmployeeManagement.Data/DbModels/EmployeeLeaveRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EmployeeManagement.Data.DbModels
+{
+   public class EmployeeLeaveRequest :BaseEntity
+    {
+        //********************************************************************************//
+
+        //Talepte bulunan kullanıcı.
+        public string RequestingEmployeeId { get; set; }
+        [ForeignKey("RequestingEmployeeId")]
+        public Employee RequestingEmployee { get; set; }
+
+        //Onaylayan kullanıcı.
+        public string ApprovedEmployeeId { get; set; }
+        [ForeignKey("ApprovedEmployeeId")]
+        public Employee ApprovedEmployee { get; set; }
+
+
+        public int EmployeeLeaveTypeId { get; set; }
+        [ForeignKey("EmployeeLeaveTypeId")]
+        public EmployeeLeaveType employeeLeaveType { get; set; }
+
+        //********************************************************************************//
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DateRequested{ get; set; }
+        public string RequestComments { get; set; }
+        public bool? Approved { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
---
+++ EmployeeManagement.Data/DbModels/EmployeeLeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Data.DbModels
+{
+    public class EmployeeLeaveType :BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int DefaultDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+    }
+}
